94e4ab1baebba9aa443a1dd71cebd89973f69e4b
Axrich <116589250+Axrich@users.noreply.github.com>
2024-08-08 15:00:48 +0500

    Update LinkedListCreator.java

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -1,9 +1,15 @@
 package com.epam.mjc.collections.list;
 
-import java.util.LinkedList;
+import java.util.Comparator;
 import java.util.List;
 
-public class LinkedListCreator {
-    public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+public class ListSorter {
+    public void sort(List<String> sourceList) {
+    }
+}
+
+class ListComparator implements Comparator<String> {
+    @Override
+    public int compare(String a, String b) {
     }
 }
