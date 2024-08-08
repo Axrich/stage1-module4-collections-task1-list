7e683e8dd6f8097bc2e6b1cf2bc00029b758b6a6
Axrich <116589250+Axrich@users.noreply.github.com>
2024-08-08 15:02:29 +0500

    Update LinkedListCreator.java

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -1,23 +1,26 @@
 package com.epam.mjc.collections.list;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
-public class LinkedListCreator {
-    public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
-        LinkedList<Integer> linkedList = new LinkedList<>();
+public class ListSorter {
+    public void sort(List<String> sourceList) {
+        Collections.sort(sourceList, new ListComparator());
 
-        for (int element : sourceList) {
+    }
 
-                if (element % 2 != 0) {
-                    linkedList.addFirst(element);
-                } else {
-                    linkedList.addLast(element);
-                }
+    class ListComparator implements Comparator<String> {
+        @Override
+        public int compare(String a, String b) {
 
+            int firstNumber = 5 * Integer.parseInt(a) * Integer.parseInt(a) + 3;
+            int secondNumber = 5 * Integer.parseInt(b) * Integer.parseInt(b) + 3;
+            if (firstNumber == secondNumber) {
+                return a.compareTo(b);
+            }
+            return Integer.compare(firstNumber, secondNumber);
         }
-
-        return linkedList;
-
-    }}
+    }
+}
