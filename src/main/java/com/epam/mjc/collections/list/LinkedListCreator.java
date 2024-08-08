a409bb294fb29d31085130aa99f3f200af36b1b0
Axrich <116589250+Axrich@users.noreply.github.com>
2024-08-08 15:02:20 +0500

    Update LinkedListCreator.java

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -1,17 +1,23 @@
 package com.epam.mjc.collections.list;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
-public class ArrayListCreator {
-    public ArrayList<String> createArrayList(List<String> sourceList) {
-        ArrayList<String> arrayList = new ArrayList<>();
-        int sourceListSize = sourceList.size();
-        for (int i = 1; i < sourceListSize; i += 3) {
-            if (i + 1 < sourceListSize) {
-                arrayList.add(sourceList.get(i + 1));arrayList.add(sourceList.get(i + 1));
-            }
+public class LinkedListCreator {
+    public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        for (int element : sourceList) {
+
+                if (element % 2 != 0) {
+                    linkedList.addFirst(element);
+                } else {
+                    linkedList.addLast(element);
+                }
+
         }
-        return arrayList;
-    }
-}
+
+        return linkedList;
+
+    }}
